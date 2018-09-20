e0bf3264f81f2a1eae693be951120ad9026ef395
Victor Landim <victorlandim5@gmail.com>
2018-09-20 14:30:03 +0000

    Prod DB added

--- config/index.js
+++ config/index.js
@@ -1,3 +1,6 @@
 module.exports = {
-    mongoUrl: `mongodb://localhost:27017/restaurants-db5`
+    mongoUrl:
+        process.env.DB_PW && process.env.DB_USER
+            ? `mongodb://${process.env.DB_USER}:${process.env.DB_PW}@ds255451.mlab.com:55451/restaurants-db5`
+            : `mongodb://localhost:27017/restaurants-db5`
 };
